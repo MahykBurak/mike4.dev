61828c9d668283f16934236bf05f44d5522f96ce
Burak Mike <burakmahyk@gmail.com>
2023-02-26 02:20:07 +0300

    changed lang

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,23 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { cx } from '@/lib/utils'
-import Header from '@/components/Header'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { cx } from '@/lib/utils';
+import Header from '@/components/Header';
 export const metadata = {
-    title: 'Burak Mike',
-    description: 'Burak Mike personal website',
-}
-const inter = Inter({ subsets: ['latin'] })
+  title: 'Burak Mike',
+  description: 'Burak Mike personal website',
+};
+const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body
-                className={cx(inter.className, 'mainAnimation mainContainer')}
-            >
-                <Header />
-                {children}
-            </body>
-        </html>
-    )
+  return (
+    <html lang="tr">
+      <body className={cx(inter.className, 'mainAnimation mainContainer')}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
 }
