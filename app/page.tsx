d724d2c9f4f7cddce2b01f9bd523b1c1ce709294
Burak Mike <burakmahyk@gmail.com>
2023-02-26 15:55:57 +0300

    refactor: remove unused files

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-import { cx } from '@/lib/utils'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
-    return <div className="text-center h-screen flex">Hello word!</div>
+  return <div className="text-center h-screen flex">Hello word!</div>;
 }
