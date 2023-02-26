898e0f07d30de0e7b2585970d26466914b045fc6
Burak Mike <burakmahyk@gmail.com>
2023-02-26 21:26:06 +0300

    fix: typescript error

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,12 +1,38 @@
+import { getNowPlaying } from '@/lib/spotify';
 import Link from 'next/link';
 import SpotifyNowPlaying from './SpotifyNowPlaying';
 
+async function nowPlaying() {
+  const response = await getNowPlaying();
+  if (response.status === 204) {
+    return { isPlaying: false } as const;
+  }
+  const data = await response.json();
+  const song = data.item;
+  const isPlaying = true;
+  const name = song.name;
+  const artistName = song.artists
+    .map((_artist: any) => _artist.name)
+    .join(', ');
+  const albumName = song.album.name;
+  const albumImage = song.album.images[2].url;
+  const songUrl = song.external_urls.spotify;
+  const songData = {
+    isPlaying,
+    name,
+    artistName,
+    albumName,
+    albumImage,
+    songUrl,
+  } as const;
+  return songData;
+}
 export default async function Footer() {
+  const data = await nowPlaying();
   return (
     <footer className="mainContainer mx-auto mb-8 flex max-w-2xl flex-col items-start justify-center pt-10">
       <hr className="border-1 mb-8 w-full border-gray-200 dark:border-gray-800" />
-      {/* @ts-expect-error Server Component */}
-      <SpotifyNowPlaying />
+      <SpotifyNowPlaying data={data} />
       <div className="grid w-full max-w-2xl grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
         <div className="flex flex-col space-y-4">
           <Link className="footerLink" href="/">
